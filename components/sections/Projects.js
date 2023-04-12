c14eedc916e1492cf5a2bfd03545a90b23d94e12
Eduardo <prog.syrius@gmail.com>
2023-04-12 13:36:01 -0300

    feat: add Projects Section

---
+++ components/sections/Projects.js
@@ -0,0 +1,20 @@
+import portfolioProjects from "@/constants/portfolioProjects";
+
+import ProjectCard from "../ProjectCard";
+
+export default function Projects() {
+  return (
+    <section className="px-40 py-20 text-primary font-primary">
+      <p className="font-semibold text-xl font-special">
+        Feito com <span className="line-through">amor</span> um{" "}
+        <span className="text-orange">teclado</span>
+      </p>
+      <h2 className="font-bold font-special text-4xl mt-3 mb-10 ">Projetos</h2>
+      <div className="flex flex-col justify-between min-h-[1200px]">
+        {portfolioProjects.map((project) => (
+          <ProjectCard project={project} />
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ constants/portfolioProjects.js
@@ -0,0 +1,31 @@
+const portfolioProjects = [
+  {
+    name: "HooperStore",
+    // imgUrl: `${exampleProject}`,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    techStack: ["Typescript", "Next.js", "Tailwindcss", "Firebase"],
+    liveLink: "#",
+    githubLink: "#",
+  },
+  {
+    name: "GainzApp",
+    // imgUrl: `${exampleProject}`,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    techStack: ["Typescript", "Next.js", "Tailwindcss", "Firebase"],
+    liveLink: "#",
+    githubLink: "#",
+  },
+  {
+    name: "DevContent",
+    // imgUrl: `${exampleProject}`,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    techStack: ["Typescript", "Next.js", "Tailwindcss", "Firebase"],
+    liveLink: "#",
+    githubLink: "#",
+  },
+];
+
+export default portfolioProjects;
